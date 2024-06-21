7f105b39456bcffdd89570e1c4be58003c7688c5
dholdaway <d.m.holdaway@googlemail.com>
2024-06-21 11:10:12 +0100

    bug fixing

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,14 @@ const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const compression = require("compression");
-const os = require('os'); 
-const fs = require('fs'); 
+const os = require("os");
+const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Update the CORS origin to allow requests from localhost
 const corsOptions = {
-  origin: ["http://localhost", "http://frontend","http://localhost:3000"],
+  origin: ["http://localhost", "http://frontend", "http://localhost:3000"],
   methods: "POST",
   allowedHeaders: "Content-Type",
   credentials: true,
@@ -28,7 +28,7 @@ app.use(helmet());
 app.use(compression());
 
 // Ensure the uploads directory exists
-const uploadDir = 'uploads';
+const uploadDir = "uploads";
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
 }
@@ -73,12 +73,12 @@ app.post(
     let odsCommand;
     switch (req.body.command) {
       case "location":
-        odsCommand = `ods_tools check --location ${filePath}`;
+        odsCommand = `ods_tools check --location "${filePath}"`;
         break;
       case "account":
         if (req.files.locationCsv && req.files.locationCsv.length > 0) {
           const locationFilePath = req.files.locationCsv[0].path;
-          odsCommand = `ods_tools check --account ${filePath} --location ${locationFilePath}`;
+          odsCommand = `ods_tools check --account "${filePath}" --location "${locationFilePath}"`;
         } else {
           return res
             .status(400)
@@ -108,3 +108,115 @@ app.set("trust proxy", true);
 app.listen(PORT, () => {
   console.log("Server is listening on port", PORT);
 });
+
+
+// const express = require("express");
+// const multer = require("multer");
+// const { exec } = require("child_process");
+// const cors = require("cors");
+// const morgan = require("morgan");
+// const helmet = require("helmet");
+// const compression = require("compression");
+// const os = require('os'); 
+// const fs = require('fs'); 
+// const app = express();
+// const PORT = process.env.PORT || 4000;
+
+// // Update the CORS origin to allow requests from localhost
+// const corsOptions = {
+//   origin: ["http://localhost", "http://frontend","http://localhost:3000"],
+//   methods: "POST",
+//   allowedHeaders: "Content-Type",
+//   credentials: true,
+// };
+
+// app.use(cors(corsOptions));
+
+// // Increase the request body size limit
+// app.use(express.json({ limit: "50mb" }));
+// app.use(express.urlencoded({ limit: "50mb", extended: true }));
+// app.use(morgan(app.get("env") === "production" ? "combined" : "dev"));
+// app.use(helmet());
+// app.use(compression());
+
+// // Ensure the uploads directory exists
+// const uploadDir = 'uploads';
+// if (!fs.existsSync(uploadDir)) {
+//   fs.mkdirSync(uploadDir);
+// }
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, uploadDir);
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + "-" + file.originalname);
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
+// app.get("/", (req, res) => {
+//   res.json({
+//     ...req.headers,
+//     hostname: os.hostname(),
+//     date: new Date().toISOString(),
+//   });
+// });
+
+// app.get("/healthcheck", (req, res) => {
+//   res.json({ status: "UP" });
+// });
+
+// app.post(
+//   "/backend/upload",
+//   upload.fields([
+//     { name: "csv", maxCount: 1 },
+//     { name: "locationCsv", maxCount: 1 },
+//   ]),
+//   (req, res) => {
+//     if (!req.files || !req.files.csv || req.files.csv.length === 0) {
+//       console.error("File upload failed.");
+//       return res.status(400).send("No file uploaded");
+//     }
+
+//     const filePath = req.files.csv[0].path;
+
+//     let odsCommand;
+//     switch (req.body.command) {
+//       case "location":
+//         odsCommand = `ods_tools check --location ${filePath}`;
+//         break;
+//       case "account":
+//         if (req.files.locationCsv && req.files.locationCsv.length > 0) {
+//           const locationFilePath = req.files.locationCsv[0].path;
+//           odsCommand = `ods_tools check --account ${filePath} --location ${locationFilePath}`;
+//         } else {
+//           return res
+//             .status(400)
+//             .send("Location file missing for account check.");
+//         }
+//         break;
+//       default:
+//         console.error("Invalid command option received.");
+//         return res.status(400).send("Invalid command option.");
+//     }
+
+//     exec(odsCommand, (error, stdout, stderr) => {
+//       console.log("stdout:", stdout);
+//       console.log("stderr:", stderr);
+//       if (error) {
+//         console.error("Error running ods_tools:", error);
+//         return res.status(500).send({ stderr: stderr });
+//       }
+//       res.send({ stdout: stdout, stderr: stderr });
+//     });
+//   }
+// );
+
+// // Trust the headers passed by the proxy
+// app.set("trust proxy", true);
+
+// app.listen(PORT, () => {
+//   console.log("Server is listening on port", PORT);
+// });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import { FaUpload } from "react-icons/fa";
 const filterResponse = (response) => {
   const lines = response.split("\n");
   const filteredLines = [];
-  
+
   for (let i = 0; i < lines.length; i++) {
     if (
       lines[i].endsWith("is not a valid oed field") ||
@@ -33,13 +33,17 @@ function App() {
   const [command, setCommand] = useState("location");
   const [response, setResponse] = useState("");
   const [history, setHistory] = useState([]);
+  const [isFileSelected, setIsFileSelected] = useState(false);
+  const [isSecondFileSelected, setIsSecondFileSelected] = useState(false);
 
   const onFileChange = (e) => {
     setFile(e.target.files[0]);
+    setIsFileSelected(!!e.target.files.length);
   };
 
   const onSecondFileChange = (e) => {
     setSecondFile(e.target.files[0]);
+    setIsSecondFileSelected(!!e.target.files.length);
   };
 
   const downloadIssues = () => {
@@ -48,7 +52,6 @@ function App() {
     const link = document.createElement("a");
     link.href = url;
 
-    // Modify this line to set the filename as per your requirement
     let downloadFileName = file.name.replace(/\.[^/.]+$/, "") + "_issues.txt";
     link.setAttribute("download", downloadFileName);
 
@@ -66,7 +69,7 @@ function App() {
     formData.append("command", command);
 
     try {
-      const result = await axios.post("http://localhost:4000/backend/upload", formData, {
+      const result = await axios.post("http://localhost/backend/upload", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
@@ -96,7 +99,7 @@ function App() {
   };
 
   return (
-    <div className="bg-gradient-to-r from-blue-200 to-blue-100 min-h-screen flex items-center justify-center">
+    <div className="bg-gradient-to-r from-blue-300 to-blue-100 min-h-screen flex items-center justify-center">
       <div className="flex bg-white max-w-1xl mx-auto p-8 shadow-xl rounded-lg text-center mt-10">
         <div className="w-1/2 bg-white p-4 overflow-y-auto">
           {history.map((item, index) => (
@@ -119,39 +122,65 @@ function App() {
               className="p-3 border rounded selectoptions w-full focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             >
               <option value="location">Location</option>
-              <option value="account">Account</option>
+              <option value="account">Account and Location</option>
             </select>
-            <button className="btn inputfile">
-              <FaUpload className="mr-2" /> Choose a file..
-            </button>
-            <input
-              type="file"
-              onChange={onFileChange}
-              className="p-3 inputchose border rounded w-full focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-            />
-            {command === "account" && (
+            {command === 'location' ? (
               <>
-                <button className="btn inputfile">
-                  <FaUpload className="mr-2" /> Choose a file..
+                <button
+                  className={`btn inputfile ${
+                    isFileSelected ? 'bg-green-500 hover:bg-green-600' : 'bg-blue-500 hover:bg-blue-600'
+                  }`}
+                  onClick={() => document.getElementById('location-file-input').click()}
+                >
+                  <FaUpload className="mr-2" /> Choose a location file
                 </button>
                 <input
                   type="file"
+                  id="location-file-input"
+                  onChange={onFileChange}
+                  className="hidden"
+                />
+              </>
+            ) : (
+              <>
+                <button
+                  className={`btn inputfile ${
+                    isFileSelected ? 'bg-green-500 hover:bg-green-600' : 'bg-blue-500 hover:bg-blue-600'
+                  }`}
+                  onClick={() => document.getElementById('account-file-input').click()}
+                >
+                  <FaUpload className="mr-2" /> Choose an account file
+                </button>
+                <input
+                  type="file"
+                  id="account-file-input"
+                  onChange={onFileChange}
+                  className="hidden"
+                />
+                <button
+                  className={`btn inputfile mt-4 ${
+                    isSecondFileSelected ? 'bg-green-500 hover:bg-green-600' : 'bg-blue-500 hover:bg-blue-600'
+                  }`}
+                  onClick={() => document.getElementById('location-file-input-2').click()}
+                >
+                  <FaUpload className="mr-2" /> Choose a location file
+                </button>
+                <input
+                  type="file"
+                  id="location-file-input-2"
                   onChange={onSecondFileChange}
-                  className="p-3 border inputchose rounded w-full focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                  placeholder="Upload location file"
-                />{" "}
+                  className="hidden"
+                />
               </>
             )}
             <div className="space-x-4">
               <button
                 onClick={onUpload}
-                className={`${
-                  response && !response.includes("File is valid.") && "w-1/2"
-                }  bg-blue-500 uploadsubmit hover:bg-blue-600 text-white py-3 px-4 rounded transform transition-transform duration-150 hover:scale-105`}
+                className="bg-blue-500 hover:bg-blue-600 text-white py-3 px-4 rounded transform transition-transform duration-150 hover:scale-105"
               >
                 Upload
               </button>
-              {response && !response.includes("File is valid.") && (
+              {response && !response.includes('File is valid.') && (
                 <button
                   onClick={downloadIssues}
                   className="w-1/2 bg-red-500 downloadred hover:bg-red-600 text-white py-3 px-4 rounded transform transition-transform duration-150 hover:scale-105"
